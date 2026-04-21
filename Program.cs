d008d9380f60a4194f6e8922cebb26a7b5f8773d
MrPravinS <pravin.shegamwar@willovate.in>
2026-04-21 19:56:25 +0530

    started dotnet

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿// Arithmetic Operator
+// + , - , * , / ,%
+
+// Asignment operator
+// += , -=, *= , /= 
+// A , B = A+B  =>  A += B
+
+// Comparison Operator 
+// < , > , >= , <= , == , != 
+
+// logical operator
+// A & B  = true when both are true 
+// A | B = When one value is true 
+
+// increment/decrement 
+  // A = 5 A++ A--
+
+  // ternary ope
+  // A= 5; B= 6
+  // A+b == 10 ? "True":"false"
+
+
+  // controll flow 
+
+
+//   int A = 10;
+//   int B = 10; 
+
+//   if(A < B)
+// {
+//     Console.WriteLine("A is greater than B");
+// }
+
+// else if(A > B)
+// {
+//     Console.WriteLine("A Is greater than B");
+
+// }
+// else
+// {
+//     Console.WriteLine("Both are equal ");
+    
+// }
+
+
+
+
+// loop What is loop 
+
